1a74679071f23e86484fa8887977ee4441a023d5
Amit Jeve <amitjeve123@gmail.com>
2022-11-04 15:51:19 +0530

    checkbox error resolved

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -1,10 +1,8 @@
-
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Data } from 'src/assets/data-interface';
 import { BuyService } from './../buy.service';
 import { DataService } from '../data.service';
-
 
 
 @Component({
@@ -19,70 +17,44 @@ export class CheckoutComponent implements OnInit{
   checkOutData:Data[]=[]
   totalAmount:number = 0
   buyArray:any=[]
-  buyActive:Boolean=true
-  checkedData:any=[]
   isValueChecked:boolean = true
-  uniqueIds:any=[]
-  i:number=0
-  j:number=0
 
   constructor(private dataservice:DataService,private route : Router, private buyservice:BuyService) {
   }
 
   ngOnInit(){
        this.checkOutData = this.dataservice.checkOutData
-
+       this.buyArray = this.checkOutData
+       this.updateTotal()
   }
 
+    isChecked(arg:any, event:any){
 
-    isChecked(arg:any){
-// console.log(this.i)
-      if(this.isValueChecked){
-        this.buyActive=true;
-        this.buyArray.push(arg)
-        // console.log(this.buyArray)
-
-        for(;this.i<this.buyArray.length;this.i++){
-          this.totalAmount= this.totalAmount + this.buyArray[this.i].price
-        }
-        // console.log(this.i)
+      if(event.target.checked){
+          if(this.buyArray.filter((e:any)=> e.id === arg.id).length ===0){
+              this.buyArray.push(arg)
+          }
         this.buyservice.buyData= this.buyArray
       }
 
-      if(!this.isValueChecked){
-        this.i=0
-        const id= arg.id
-        const indexFin  = this.buyArray.findIndex((e:any)=>{
-          if(e.id === id){
-            return id;
-          }
-        })
-        if(indexFin != -1){
-          console.log(this.totalAmount)
-          console.log(this.buyservice.buyData[indexFin].price)
-          this.totalAmount = this.totalAmount - this.buyservice.buyData[indexFin].price
-          console.log('Unchecked',indexFin)
-          console.log(this.totalAmount)
-            if(this.totalAmount<0){
-              this.totalAmount=0;
-            }
-
-
-        }
-
-        this.buyArray.splice(indexFin,1)
+      if(!event.target.checked){
+        this.buyArray= this.buyArray.filter((e:any)=>e.id != arg.id)
+        console.log(this.buyArray)
         this.buyservice.buyData = this.buyArray
-        console.log(this.buyservice.buyData)
 
-
-      }
+    }
+    this.updateTotal()
     }
 
  onSelected(arg:any){
-
   this.route.navigate(['/buy'])
+ }
 
-
+ updateTotal(){
+  let temp=0
+  for(let i=0;i<this.buyArray.length;i++){
+      temp = temp + this.buyArray[i].price
+  }
+  this.totalAmount= temp;
  }
 }
-
